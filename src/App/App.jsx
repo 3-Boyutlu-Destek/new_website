4d60291f66402385a45c30c2975e581f0cbc31fe
kahasan <kahraman.h@outlook.com>
2020-04-26 21:12:15 +0300

    Nasıl destek olabilirsiniz kısmı eklendi

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,82 +4,100 @@ import { history } from '@/_helpers';
 import { authenticationService } from '@/_services';
 import { PrivateRoute } from '@/_components';
 import { HomePage } from '@/HomePage';
-//import { style } from '@/App';
+import { style } from '@/App';
 
-const titleStyle={
-    color: 'black',
-     fontWeight: '600',
-     padding: '5px',
-     fontSize: '20px',
-     marginLeft:'20px'
- }
- const destekOlTitleStyle = {
-    color: 'white',
-    fontWeight: '600',
-    padding: '5px',
-    fontSize: '18px', 
-    marginLeft:'20px',
-    backgroundColor: '#FF4933',
-    borderRadius : '25px',
-    width: '140px',
-    textAlign: 'center'
- }
- const talepEtTitleStyle = {
-    color: 'white',
-    fontWeight: '600',
-    padding: '5px',
-    fontSize: '18px', 
-    marginLeft:'20px',
-    backgroundColor: 'black',
-    borderRadius : '25px',
-    width: '120px',
-    textAlign: 'center'
- }
+const titleStyle = {
+  color: 'black',
+  fontWeight: '600',
+  padding: '5px',
+  fontSize: '20px',
+  marginLeft: '20px',
+};
+const destekOlTitleStyle = {
+  color: 'white',
+  fontWeight: '600',
+  padding: '5px',
+  fontSize: '18px',
+  marginLeft: '20px',
+  backgroundColor: '#FF4933',
+  borderRadius: '25px',
+  width: '140px',
+  textAlign: 'center',
+};
+const talepEtTitleStyle = {
+  color: 'white',
+  fontWeight: '600',
+  padding: '5px',
+  fontSize: '18px',
+  marginLeft: '20px',
+  backgroundColor: 'black',
+  borderRadius: '25px',
+  width: '120px',
+  textAlign: 'center',
+};
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            currentUser: null
-        };
-    }
+    this.state = {
+      currentUser: null,
+    };
+  }
 
-    componentDidMount() {
-        authenticationService.currentUser.subscribe(x => this.setState({ currentUser: x }));
-    }
+  componentDidMount() {
+    authenticationService.currentUser.subscribe((x) =>
+      this.setState({ currentUser: x })
+    );
+  }
 
-    render() {
-        const { currentUser } = this.state;
-        
+  render() {
+    const { currentUser } = this.state;
 
-        return (
-            <Router history={history}>
-                <div>
-                    {
-                        <nav className="navbar navbar-expand navbar-dark" style={{width:'95%'}}>
-                            <Link to="/" className="nav-item" style={titleStyle}><img className="nav-item nav-link" src="public/logo.png" alt="Ana Sayfa"></img></Link>
-                            <div className="navbar-nav" style={{marginLeft:'auto'}}>
-                                <div style={titleStyle} >Hakkımızda</div>
-                                <a style={titleStyle} className="nav-item" target="_blank" >Destekçiler</a>
-                                <a  className="nav-item" style={titleStyle} >Ürünler</a>
-                                <a  className="nav-item" style={destekOlTitleStyle} >Destek Ol</a>
-                                <a  className="nav-item" style={talepEtTitleStyle} >Talep Et</a>
-                            </div>
-                        </nav>
-                    }
-                    <div className="jumbotron" style={{backgroundColor:'white'}}> 
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-md-12">
-                                    <PrivateRoute exact path='/' component={HomePage} />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+    return (
+      <Router history={history}>
+        <div>
+          {
+            <nav
+              className="navbar navbar-expand navbar-dark"
+              style={{ width: '95%' }}
+            >
+              <Link to="/" className="nav-item" style={titleStyle}>
+                <img
+                  className="nav-item nav-link"
+                  src="public/logo.png"
+                  alt="Ana Sayfa"
+                ></img>
+              </Link>
+              <div className="navbar-nav" style={{ marginLeft: 'auto' }}>
+                <div style={titleStyle}>Hakkımızda</div>
+                <a style={titleStyle} className="nav-item" target="_blank">
+                  Destekçiler
+                </a>
+                <a className="nav-item" style={titleStyle}>
+                  Ürünler
+                </a>
+                <a className="nav-item" style={destekOlTitleStyle}>
+                  Destek Ol
+                </a>
+                <a className="nav-item" style={talepEtTitleStyle}>
+                  Talep Et
+                </a>
+              </div>
+            </nav>
+          }
+          <div className="" style={{ backgroundColor: 'white' }}>
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <PrivateRoute exact path="/" component={HomePage} />
                 </div>
-            </Router>
-        );
-    }
+              </div>
+            </div>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
-export { App }; 
+export { App };
--- src/App/index.js
+++ src/App/index.js
@@ -1,2 +1,2 @@
 export * from './App';
-//export * from './style';
+export * from './style';
