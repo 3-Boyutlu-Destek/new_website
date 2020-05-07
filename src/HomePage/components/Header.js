71e10b68adc24e46fe9a118dec5c899f086f6240
kahasan <kahraman.h@outlook.com>
2020-05-07 22:39:05 +0300

    Header boyutları değiştirildi

--- src/HomePage/components/Header.js
+++ src/HomePage/components/Header.js
@@ -5,18 +5,47 @@ function toggle() {
   e[0].classList.toggle('switch');
 }
 
+window.onscroll = function () {
+  scrollFunc();
+};
+
+function scrollFunc() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById('logo').src = 'logo-text.svg';
+    document.getElementsByClassName('Header-nav')[0].classList.add('scroll');
+    document
+      .getElementsByClassName('Header-buttons')[0]
+      .classList.add('scroll');
+  } else {
+    document.getElementById('logo').src = 'logo.svg';
+    document.getElementsByClassName('Header-nav')[0].classList.remove('scroll');
+    document
+      .getElementsByClassName('Header-buttons')[0]
+      .classList.remove('scroll');
+  }
+}
+
 function Header() {
   return (
     <header className="Header">
       <nav className="Header-nav">
         <a href="/" className="Header-nav-link">
-          <img className="Header-nav-link-img" src="logo.svg" alt="Ana Sayfa"></img>
+          <img
+            id="logo"
+            className="Header-nav-link-img"
+            src="logo.svg"
+            alt="Ana Sayfa"
+          ></img>
         </a>
-        <div className="Header-switch" onClick={toggle} style={{ cursor: 'pointer' }}>
+        <div
+          className="Header-switch"
+          onClick={toggle}
+          style={{ cursor: 'pointer' }}
+        >
           <img className="Header-switch-icon" src="menu.svg" />
         </div>
         <div className="Header-buttons">
-        { /*<a href="" className="Header-buttons-link">
+          {/*<a href="" className="Header-buttons-link">
             Hakkımızda
           </a>
           <a href="" className="Header-buttons-link">
@@ -24,10 +53,12 @@ function Header() {
           </a>
           <a href="" className="Header-buttons-link">
             Ürünler
-  </a> */}  
-          <a href="https://docs.google.com/forms/d/e/1FAIpQLScR4-5CE7VXJwTO84wvjLHxn11vtnZrip3bMr-ncHKKo2GBOw/viewform"
-             className="Header-buttons-link primary" 
-             target="_blank">
+  </a> */}
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLScR4-5CE7VXJwTO84wvjLHxn11vtnZrip3bMr-ncHKKo2GBOw/viewform"
+            className="Header-buttons-link primary"
+            target="_blank"
+          >
             Destek Ol
           </a>
           <a
